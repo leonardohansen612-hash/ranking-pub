6cd5c88e34809138f36ca53df221031fee7d7eb2
leonardohansen612-hash <leonardohansen612@gmail.com>
2026-08-28 19:40:38 -0300

    Add files via upload

--- main.jsx
+++ main.jsx
@@ -14,7 +14,7 @@ import {
   updateDoc,
   writeBatch,
 } from 'firebase/firestore';
-import { db } from './firebase';
+import { db } from './firebase.js';
 import './styles.css';
 
 const todayKey = () => {
